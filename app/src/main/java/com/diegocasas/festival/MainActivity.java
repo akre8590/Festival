378313ab6cf82350a3be13028c05b9a0db39a3e6
akre8590 <akre8590@gmail.com>
2018-10-25 08:57:55 -0500

    Initial commit

---
+++ app/src/main/java/com/diegocasas/festival/MainActivity.java
@@ -0,0 +1,44 @@
+package com.diegocasas.festival;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+    FirebaseAuth.AuthStateListener mAuthListner;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListner);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListner = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser()==null)
+                {
+                    startActivity(new Intent(MainActivity.this, SingInActivity.class));
+                }
+            }
+        };
+    }
+
+    public void test(View view){
+        mAuth.signOut();
+    }
+}
